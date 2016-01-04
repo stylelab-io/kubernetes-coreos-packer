e0d299272798e9732dc61d49299a7e7fe7467969
Steven Wirges <steven.wirges@stylelounge.de>
2016-01-04 14:17:11 +0100

    rearranged file structure

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
-VERSION_ID := 0.1.0
+VERSION_ID := 0.1.9
 CHANNEL    := alpha
-KUBE_VERSION := 1.1.0
+KUBE_VERSION := 1.1.2
 
 build-all:
 	packer build -var-file=vars.json corekube.json
@@ -8,8 +8,8 @@ build-all:
 build-virtualbox:
 	packer build -only=virtualbox-iso -var-file=vars.json corekube.json
 build-googlecompute:
-	packer build -only=googlecompute -var-file=vars.json corekube.json 
+	packer build -only=googlecompute -var-file=vars.json corekube.json
 
 install:
-	vagrant box add --force --provider=parallels --name stylelab-io/corekube-$(KUBE_VERSION)-$(VERSION_ID) builds/parallels/coreos-$(CHANNEL).box
-	vagrant box add --force --provider=virtualbox --name stylelab-io/corekube-$(KUBE_VERSION)-$(VERSION_ID) builds/virtualbox/coreos-$(CHANNEL).box
+	vagrant box add --force --provider=parallels --name stylelab-io/corekube-$(KUBE_VERSION)-$(VERSION_ID) builds/$(CHANNEL)/parallels/coreos-$(CHANNEL).box
+	vagrant box add --force --provider=virtualbox --name stylelab-io/corekube-$(KUBE_VERSION)-$(VERSION_ID) builds/$(CHANNEL)/virtualbox/coreos-$(CHANNEL).box
--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -1,5 +1,5 @@
 ################################################
-#  Installs Kubernetes files to /opt/kubernetes
+#  Installs Kubernetes files to /opt/bin
 #  and adds the make-cert scripts
 #
 
@@ -10,12 +10,11 @@ tar -xvf kubernetes.tar.gz
 sudo mkdir -p /opt/kubernetes
 tar -xvf ./kubernetes/server/kubernetes-salt.tar.gz
 tar -xvf ./kubernetes/server/kubernetes-server-linux-amd64.tar.gz
-sudo cp ./kubernetes/server/bin/hyperkube /opt/kubernetes/hyperkube
-sudo cp ./kubernetes/server/bin/kubectl /opt/kubernetes/kubectl
-sudo cp ./kubernetes/server/bin/kubelet /opt/kubernetes/kubelet
+sudo cp ./kubernetes/server/bin/hyperkube /opt/bin/hyperkube
+sudo cp ./kubernetes/server/bin/kubectl /opt/bin/kubectl
+sudo cp ./kubernetes/server/bin/kubelet /opt/bin/kubelet
 sudo cp ./kubernetes/saltbase/salt/generate-cert/make-ca-cert.sh ./make-ca-cert.sh
 sudo cp ./kubernetes/saltbase/salt/generate-cert/make-cert.sh ./make-cert.sh
-sudo chmod 775 /opt/kubernetes
-echo "export PATH=/opt/kubernetes:\$PATH" > ~/.bashrc
+echo "export PATH=/opt/bin:\$PATH" > ~/.bashrc
 
 rm -rf kubernetes kubernetes.tar.gz
