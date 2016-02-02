48eb013d705e785fca5f3c54aca7bba63b05c574
Steven Wirges <steven.wirges@stylelounge.de>
2016-02-02 17:07:19 +0100

    fixed virtualbox bug

--- Makefile
+++ Makefile
@@ -1,9 +1,12 @@
 build:
 	./build.sh
 
-build-virtualbox: ./build.sh virtualbox-iso
-build-vmware: ./build.sh  vmware-iso
-build-google: ./build.sh googlecompute
+build-virtualbox:
+	./build.sh virtualbox-iso
+build-vmware:
+	./build.sh vmware-iso
+build-google:
+	./build.sh googlecompute
 
 install:
 	vagrant box add --force --provider=parallels --name stylelab-io/corekube-$(CHANNEL)--$(KUBE_VERSION) builds/parallels/coreos-$(CHANNEL).box
--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -1,5 +1,5 @@
 ################################################
-#  Installs Kubernetes files to /opt/kubernetes
+#  Installs Kubernetes files to /opt
 #  and adds the make-cert scripts
 #
 
@@ -7,15 +7,13 @@ version=${KUBE_VERSION:-v1.0.6}
 
 wget https://github.com/kubernetes/kubernetes/releases/download/${version}/kubernetes.tar.gz
 tar -xvf kubernetes.tar.gz
-sudo mkdir -p /opt/kubernetes
 tar -xvf ./kubernetes/server/kubernetes-salt.tar.gz
 tar -xvf ./kubernetes/server/kubernetes-server-linux-amd64.tar.gz
-sudo cp ./kubernetes/server/bin/hyperkube /opt/kubernetes/hyperkube
-sudo cp ./kubernetes/server/bin/kubectl /opt/kubernetes/kubectl
-sudo cp ./kubernetes/server/bin/kubelet /opt/kubernetes/kubelet
+sudo cp ./kubernetes/server/bin/hyperkube /opt/hyperkube
+sudo cp ./kubernetes/server/bin/kubectl /opt/kubectl
+sudo cp ./kubernetes/server/bin/kubelet /opt/kubelet
 sudo cp ./kubernetes/saltbase/salt/generate-cert/make-ca-cert.sh ./make-ca-cert.sh
 sudo cp ./kubernetes/saltbase/salt/generate-cert/make-cert.sh ./make-cert.sh
-sudo chmod 775 /opt/kubernetes
-echo "export PATH=/opt/kubernetes:\$PATH" > ~/.bashrc
+echo "export PATH=/opt:\$PATH" > ~/.bashrc
 
 rm -rf kubernetes kubernetes.tar.gz
